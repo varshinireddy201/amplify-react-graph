b7e12d6b2c7c8c7d9ffa6d1cb9667da49066753e
varshinireddy201 <varshinireddy201@gmail.com>
2023-03-26 16:57:13 -0700

    added auth

---
+++ src/aws-export.js
@@ -0,0 +1,27 @@
+const awsmobile = {
+    "aws_project_region": "us-east-2",
+    "aws_cognito_identity_pool_id": "us-east-2:0982d428-568c-498d-9d96-98dddfe0d54f",
+    "aws_cognito_region": "us-east-2",
+    "aws_user_pools_id": "us-east-2_9lhilCCKU",
+    "aws_user_pools_web_client_id": "62a66d76acde7bv6i0u29r435p",
+    "oauth": {},
+    "aws_cognito_username_attributes": [],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+
+export default awsmobile;
